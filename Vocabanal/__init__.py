50cedf6d452b2768f31950a9c9cefd225dbc773e
Fabrizio Miano <fabriziomiano@gmail.com>
2019-09-09 20:03:26 +0200

    add variables

--- Vocabanal/__init__.py
+++ Vocabanal/__init__.py
@@ -3,6 +3,13 @@ The flask application package.
 """
 
 from flask import Flask
-app = Flask(__name__, static_url_path='/static')
-app.secret_key = "493wtgZewc9un"
-import Vocabanal.views
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+CWD = os.getcwd()
+UPLOAD_FOLDER = os.path.join(CWD, 'Vocabanal/public/uploads')
+RESULTS_FOLDER = os.path.join(CWD, 'Vocabanal/static/img/results/fullsize')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['RESULTS_FOLDER'] = RESULTS_FOLDER
+from Vocabanal import views, api
