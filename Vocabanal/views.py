8916f836c61abf61ebec46e71d9be81a8fde46d3
Fabrizio Miano <fabriziomiano@gmail.com>
2019-09-09 20:05:45 +0200

    Move APIs; add error 404 view

--- Vocabanal/views.py
+++ Vocabanal/views.py
@@ -2,31 +2,20 @@
 Routes and views for the flask application.
 """
 from datetime import datetime
-from flask import render_template, send_from_directory, url_for
+from flask import render_template
 from Vocabanal import app
+ALLOWED_EXTENSIONS = "pdf, tex"
 
-#
-# @app.errorhandler(404)
-# def page_not_found(e):
-#     # note that we set the 404 status explicitly
-#     return render_template('404.jade'), 404
+
+@app.errorhandler(404)
+def page_not_found():
+    return render_template('404.html'), 404
 
 
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home',
-        year=datetime.now().year,
-    )
-
-
-@app.route('/api/download')
-def download():
-    """Return the files produced"""
-    # TODO Call a function to zip the files produced and return the archive
     return render_template(
         'index.html',
         title='Home',
