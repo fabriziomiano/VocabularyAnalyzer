db350c7e8279bda13e74486701a9754548c3e003
Fabrizio Miano <fabriziomiano@gmail.com>
2019-09-09 20:07:04 +0200

    add data and plot modules

---
+++ Vocabanal/modules/data.py
@@ -0,0 +1,107 @@
+from collections import Counter
+from Vocabanal import app
+
+
+def kwords_count(corpus):
+    """
+    Perfom word count on a given corpus
+    Return a descedant sorted list of tuples(word, count)
+    :param corpus: list
+    :return: list
+    """
+    app.logger.info("Running keywords count")
+    return Counter(corpus.split()).most_common()
+
+
+def get_adverbs(doc):
+    """
+    Given a spacy-parsed document
+    return a list with all the adverbs in it.
+    :param doc: spacy.tokens.doc.Doc parsed document
+    :return: list
+    """
+    return [
+        token.lower_ for sent in doc.sents
+        for token in sent if token.pos_ == "ADV"
+    ]
+
+
+def get_data(nlp, doc):
+    """
+    Get doc main POS data and entities
+    with their types from a given corpus
+    :param nlp: spacy.lang.en.English
+    :param doc: spacy.tokens.doc.Doc object
+    :return: dict
+    """
+    app.logger.info("Getting data from doc")
+    nouns, adjectives = [], []
+    adverbs, verbs = [], []
+    entities, entity_types = [], []
+    for sent in doc.sents:
+        for token in sent:
+            if not token.is_oov and len(token.text) > 1:
+                if token.pos_ == "ADV":
+                    adverbs.append(token.lower_)
+                if token.pos_ == "VERB" and not token.is_stop:
+                    verbs.append(token.lemma_)
+                if token.pos_ == "ADJ":
+                    adjectives.append(token.lower_)
+                if token.pos_ == "NOUN":
+                    nouns.append(token.lower_)
+        subdoc = nlp(sent.text)
+        for ent in subdoc.ents:
+            if len(ent.text) > 2:
+                entities.append(ent.text)
+                entity_types.append(ent.label_)
+    adverbs_data = Counter(adverbs).most_common()
+    verbs_data = Counter(verbs).most_common()
+    nouns_data = Counter(nouns).most_common()
+    adjectives_data = Counter(adjectives).most_common()
+    entities_data = Counter(entities).most_common()
+    entity_types_data = Counter(entity_types).most_common()
+    data = {
+        "adverbs": adverbs_data,
+        "verbs": verbs_data,
+        "nouns": nouns_data,
+        "adjectives": adjectives_data,
+        "entities": entities_data,
+        "entity_types": entity_types_data
+    }
+    return data
+
+
+def normalize_data(data):
+    """
+    Normalize data to the sum of the element in the set
+    :param data: dict
+    :return: dict
+
+    Ex.:
+    data_in = {
+        'verbs': [('work', 2)],
+        'nouns': [('painter', 1)],
+        'entities': [
+            ('the Ministry of Justice', 1),
+            ('Rome', 1),
+            ('10/09/2012', 1)
+        ]
+    }
+
+    data_out = {
+        'verbs': [('work', 200.0)],
+        'nouns': [('painter', 100.0)],
+        'adjectives': [],
+        'entities': [
+            ('the Ministry of Justice', 25.0),
+            ('Italy', 'GPE', 25.0),
+            ('Rome', 'GPE', 25.0),
+            ('10/09/2012', 25.0)
+        ]
+    }
+    """
+    normalized_data = {
+        key: [(el[0], (el[1] / len(value)) * 100) for el in value]
+        for key, value in data.items()
+    }
+    return normalized_data
---
+++ Vocabanal/modules/plot.py
@@ -0,0 +1,106 @@
+import os
+import seaborn as sns
+from Vocabanal import app
+from Vocabanal.utils.constants import ENTITY_ABBRV_MAP
+from matplotlib import pyplot as plt
+
+
+def save_barplot(data, labels, n_max, path, title):
+    """
+    Save bar plot of given data in format list(tuples)
+
+    :param data: list of tuples
+    :param labels: list: x and y axis labels in this order
+    :param n_max: int: max number of elements
+    :param path: str: output file path
+    :param title: str
+    :return: None
+    """
+    x, y = zip(*data)
+    sns.set(style="whitegrid")
+    plt.figure(figsize=(n_max, n_max / 2))
+    ax = sns.barplot(
+        list(y)[:n_max],
+        list(x)[:n_max],
+        palette="Blues_d")
+    ax.set_title(title, fontsize=18)
+    plt.xticks(fontsize=18)
+    plt.xticks(fontsize=18)
+    plt.xlabel(labels[1], fontsize=18)
+    plt.ylabel(labels[0], fontsize=18, labelpad=20, rotation=90)
+    if labels[0] == "Entity Types":
+        write_entitytypes_legend(data, ax)
+    plt.savefig(path)
+
+
+def write_entitytypes_legend(data, ax):
+    """
+    Write the entity type meaning by using ENTITY_ABBRV_MAP,
+    depending on which entities are present in the data.
+    :param data:
+    :param ax: plt axes object
+    :return: None
+    """
+    app.logger.info("Writing legend on Entity Types plot")
+    legend_text = ""
+    for k, v in ENTITY_ABBRV_MAP.items():
+        for ent_type in data:
+            if k in ent_type[0]:
+                legend_text += "{}: {}\n".format(k, v)
+    plt.text(
+        0.65, 0.05, legend_text, fontsize=16,
+        transform=ax.transAxes
+    )
+
+
+def plot_pos(data, out_dir_name, n_max_words, type_pos):
+    """
+    Save a bar plot of the adverb count
+    :param data: list
+    :param out_dir_name: str
+    :param n_max_words: int
+    :param type_pos: str
+    :return: None
+    """
+    app.logger.info("Making plot for {}".format(type_pos))
+    if n_max_words < len(data):
+        plot_title = (
+                "Top {} ".format(n_max_words) + type_pos +
+                " out of {} different ones".format(len(data))
+        )
+    else:
+        plot_title = (
+            "The {} different ".format(len(data)) +
+            type_pos + " found"
+        )
+    plot_labels = [type_pos, "% of occurrence"]
+    plot_fp = os.path.join(out_dir_name, type_pos + ".png")
+    save_barplot(
+        data,
+        plot_labels,
+        n_max_words,
+        plot_fp,
+        title=plot_title
+    )
+
+
+def plot_kwords(kwords_data, out_dir_name, n_max_words):
+    """
+    Save a bar plot of the keywords count
+    :param kwords_data: list
+    :param out_dir_name: str
+    :param n_max_words: int
+    :return: None
+    """
+    app.logger.info(
+        "Making plot for the top {} keywords".format(n_max_words)
+    )
+    kwords_labels = ["Keywords", "Counts"]
+    kwords_plot_fp = os.path.join(out_dir_name, "kwords_count.png")
+    save_barplot(
+        kwords_data,
+        kwords_labels,
+        n_max_words,
+        kwords_plot_fp,
+        title="Top {} Keywords".format(n_max_words)
+    )
