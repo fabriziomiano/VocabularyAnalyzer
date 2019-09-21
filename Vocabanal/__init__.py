749294b76c0bcf732bd95cccf152a4076ab6c71a
Fabrizio Miano <fabriziomiano@gmail.com>
2019-09-22 01:52:30 +0200

    fix path; nuances

--- Vocabanal/__init__.py
+++ Vocabanal/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24)
 CWD = os.getcwd()
 UPLOAD_FOLDER = os.path.join(CWD, 'Vocabanal/public/uploads')
-RESULTS_FOLDER = os.path.join(CWD, 'Vocabanal/static/img/results/fullsize')
+RESULTS_FOLDER = os.path.join(CWD, 'Vocabanal/static/img/results')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['RESULTS_FOLDER'] = RESULTS_FOLDER
-from Vocabanal import views, api
+from Vocabanal import views, routes
