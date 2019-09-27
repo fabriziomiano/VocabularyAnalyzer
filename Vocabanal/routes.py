d32a008e663091bdecfd82f6bd95469ac175b537
Fabrizio Miano <fabriziomiano@gmail.com>
2019-09-27 14:23:29 +0200

    Make analyze and serve_plots functions, not APIs

--- Vocabanal/routes.py
+++ Vocabanal/routes.py
@@ -3,35 +3,16 @@ Routes and APIs for the flask application.
 """
 import os
 import shutil
-import spacy
-from io import BytesIO
 from uuid import uuid4
 from zipfile import ZipFile, ZIP_DEFLATED
 from werkzeug.utils import secure_filename
-from flask import (
-    request, flash, redirect, make_response,
-    url_for, jsonify, send_from_directory
-)
-from pdfminer.pdfparser import PDFSyntaxError
 from Vocabanal import app, UPLOAD_FOLDER, RESULTS_FOLDER
-from Vocabanal.classes.Text import TextPreprocessor
-from Vocabanal.modules.data import get_data, normalize_data, kwords_count
-from Vocabanal.modules.plot import plot_pos, plot_kwords
-from Vocabanal.utils.constants import ALLOWED_EXTENSIONS
-from Vocabanal.utils.misc import (
-    extract_text, create_nonexistent_dir, save_wordcloud,
-    b64str_from_path
+from Vocabanal.modules.data import analyze
+from Vocabanal.utils.misc import create_nonexistent_dir, allowed_file
+from flask import (
+    request, flash, redirect,
+    jsonify, send_from_directory
 )
-
-
-def allowed_file(filename):
-    """
-    Validate the filename
-    :param filename: str
-    :return: bool
-    """
-    return "." in filename and \
-           filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/api/upload", methods=["POST"])
@@ -53,59 +34,7 @@ def upload():
             filepath = os.path.join(updir_path, filename)
             app.logger.info("Saving {} to {}".format(filename, filepath))
             file.save(filepath)
-            return redirect(url_for(
-                "analyze", request_uuid=request_uuid, filename=filename)
-            )
-
-
-@app.route("/api/analyze/<request_uuid>, <filename>", methods=["GET"])
-def analyze(request_uuid, filename):
-    filepath = os.path.join(UPLOAD_FOLDER, request_uuid, filename)
-    n_max_words = 15
-    try:
-        with open(filepath, "rb") as file_in:
-            pdf_byte_content = BytesIO(file_in.read())
-    except PDFSyntaxError as e:
-        return make_response(
-            jsonify(status="KO", message=e),
-            400)
-    corpus = extract_text(pdf_byte_content)
-    app.logger.warning("Loading spaCy English model. This may take up to 1 minute")
-    # nlp = spacy.load("en_core_web_md")
-    nlp = spacy.load("en_core_web_sm")
-    app.logger.warning("Model loaded")
-    doc = nlp(corpus)
-    doc_data = get_data(nlp, doc)
-    norm_data = normalize_data(doc_data)
-    results_dir = os.path.join(RESULTS_FOLDER, request_uuid)
-    create_nonexistent_dir(results_dir)
-    save_wordcloud(corpus, results_dir)
-    for pos in norm_data.keys():
-        if len(norm_data[pos]) != 0:
-            plot_pos(norm_data[pos], results_dir, n_max_words, type_pos=pos)
-    tp = TextPreprocessor(corpus)
-    cleaned_text = tp.preprocess()
-    kwords_data = kwords_count(cleaned_text)
-    if len(kwords_data) != 0:
-        plot_kwords(kwords_data, results_dir, n_max_words)
-    else:
-        app.logger.warning("No keywords found in the provided PDF")
-    return redirect(url_for("serve_plots", request_uuid=request_uuid))
-
-
-@app.route("/api/serve_plots/<request_uuid>", methods=["GET"])
-def serve_plots(request_uuid):
-    results_dir = os.path.join(RESULTS_FOLDER, request_uuid)
-    results = dict()
-    for i, plot in enumerate(os.listdir(results_dir)):
-        plot_path = os.path.join(results_dir, plot)
-        plot_str = b64str_from_path(plot_path)
-        if isinstance(plot_str, Exception):
-            continue
-        else:
-            results[i] = plot_str
-    return make_response(
-        jsonify(uuid=request_uuid, status="OK", results=results), 200)
+            return analyze(request_uuid, filename)
 
 
 @app.route("/api/download/<request_uuid>")
